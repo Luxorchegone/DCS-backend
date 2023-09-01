dad26163c7517c90bf27ba002f47a6f0d9110151
Luxorchegone <luxorchegone@gmail.com>
2023-09-01 18:14:30 +0300

    Фикс сраных CORS

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [/^(.*)/],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     preflightContinue: false,
     optionsSuccessStatus: 200,
     credentials: true,
     allowedHeaders:
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization, X-Forwarded-for, X-Api-Key',
   });
   //TODO: донастроить CORS
   app.useGlobalPipes(new ValidationPipe());
