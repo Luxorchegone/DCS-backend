56433f80dfb08eafc00cea8dd5807bfb4c8f0202
Vladimir <luxorchegone@gmail.com>
2024-06-06 20:24:35 +0300

    Fix domain address

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://garnet-dcs.tech/'],
+    origin: ['https://garnet-dcs.ru/', 'http://localhost:5173'],
     methods: ['GET', 'HEAD', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     preflightContinue: false,
     optionsSuccessStatus: 200,
